5e4d20bb7de72a412acf7fd5dd90c47dbd284512
Alexandra Pickle <alexandra.pickle@gmail.com>
2024-01-03 15:03:03 -0600

    minor edits

--- projects/mfe1/src/app/state/product.store.ts
+++ projects/mfe1/src/app/state/product.store.ts
@@ -10,18 +10,17 @@ import {
 import { ProductService } from '../mfe1/product.service';
 import { Product } from '../mfe1/product.model';
 
-//  signalStore is  function provides Signal store which is basically a service
-//  providing Signals
+//  signalStore is  function provides Signal store which is basically a service providing Signals
 
 export const ProductStore = signalStore(
   //  We get a class which is a service, and this class can be provided in root or at component level,
   //  at routing level, or when bootstrapping the application
   { providedIn: 'root' },
   withState({ products: new Array<Product>() }),
-  withHooks({ onInit(store) {} }),
   withComputed((state) => {
     return { products: state.products };
   }),
+  withHooks({ onInit(store) {} }),
   withMethods((state) => {
     const productService = inject(ProductService);
 
